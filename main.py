988dc4f62fec6ade2064d09596b3e3fde3d48402
evswanson <evanswanson98@gmail.com>
2024-03-10 19:53:54 -0500

    created Cell class. Moved graphics and cell to seperate files and import to main

--- main.py
+++ main.py
@@ -1,51 +1,17 @@
-from tkinter import Tk, BOTH, Canvas
+from cell import Cell
+from graphics import Line, Point, Window
 
 
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Window")
-        self.__root.configure(width=width, height=height)
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(self.__root)
-        self.canvas.pack()
-        self.is_running = False
 
-    
-    def redraw(self):
-        print('redraw')
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
-            self.redraw()
-    
-    def close(self):
-        self.is_running = False
         
-
-class Point:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-
-class Line:
-    def __init__(self, p1:Point, p2:Point):
-        self.__p1 = p1
-        self.__p2 = p2
-
-    def draw(self, canvas:Canvas, fill_color):
-        canvas.create_line(self.__p1.x, self.__p1.y, 
-                           self.__p2.x, self.__p2.y, 
-                           fill=fill_color, width=2
-                           )
-        canvas.pack()
-
-
 def main():
     win = Window(800, 600)
+    p1 = Point(2, 20)
+    p2 = Point(50, 16)
+    line = Line(p1, p2)
+    win.draw_line(line, "black")
+    cell = Cell(win)
+    cell.draw(30, 100, 90, 150)
     win.wait_for_close()
 
 if __name__ == '__main__':
