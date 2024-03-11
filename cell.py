83e5eb84392996b8020517770ea02de5c1986b2d
evswanson <evanswanson98@gmail.com>
2024-03-10 20:22:32 -0500

    created a method to draw a line from the center of one cell to another

--- cell.py
+++ cell.py
@@ -30,3 +30,16 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+    
+    # Draw path between 2 cells from one center to another
+    def draw_move(self, to_cell, undo=False):
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        x_mid = (self._x2 + self._x1) / 2
+        y_mid = (self._y2 + self._y1) / 2
+        to_x_mid = (to_cell._x2 + to_cell._x1) / 2
+        to_y_mid = (to_cell._y2 + to_cell._y1) / 2
+
+        line = Line(Point(x_mid, y_mid), Point(to_x_mid, to_y_mid))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -6,12 +6,20 @@ from graphics import Line, Point, Window
         
 def main():
     win = Window(800, 600)
+
     p1 = Point(2, 20)
     p2 = Point(50, 16)
     line = Line(p1, p2)
     win.draw_line(line, "black")
+
     cell = Cell(win)
     cell.draw(30, 100, 90, 150)
+    
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+    
+    c.draw_move(cell)
     win.wait_for_close()
 
 if __name__ == '__main__':
