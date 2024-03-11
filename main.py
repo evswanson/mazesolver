8ee290ce527f015e312caad72b63856659a75c1e
evswanson <evanswanson98@gmail.com>
2024-03-10 21:16:54 -0500

    created maze class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cell import Cell
 from graphics import Line, Point, Window
+from maze import Maze
 
 
 
@@ -7,19 +8,7 @@ from graphics import Line, Point, Window
 def main():
     win = Window(800, 600)
 
-    p1 = Point(2, 20)
-    p2 = Point(50, 16)
-    line = Line(p1, p2)
-    win.draw_line(line, "black")
-
-    cell = Cell(win)
-    cell.draw(30, 100, 90, 150)
-    
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
-    
-    c.draw_move(cell)
+    Maze(0, 0, 2, 4, 2, 3, win)
     win.wait_for_close()
 
 if __name__ == '__main__':
---
+++ maze.py
@@ -0,0 +1,59 @@
+import time
+
+from cell import Cell
+
+
+# A list of lists to hold cells
+class Maze:
+    def __init__(self, x1, y1, 
+                 num_rows, num_cols,
+                 cell_size_x, cell_size_y,
+                 win):
+        # how many pixels from the top and left the maze should start from side of window
+        self._x1 = x1
+        self._y1 = y1
+        # number of rows and columns
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        # how big the cells should be
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
+    
+    def _create_cells(self):
+        for row in range(self._num_rows):
+            col_cells = []
+            for col in range(self._num_cols):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        
+        for row in range(self._num_rows):
+            for col in range(self._num_cols):
+                self._draw_cell(col, row)
+    
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        if i == 0:
+            x1 = self._x1
+            x2 = self._cell_size_x
+        else:
+            x1 = self._cell_size_x * i
+            x2 = self._cell_size_x * (i+1)
+        if j == 0:
+            y1 = self._y1
+            y2 = self._cell_size_y
+        else:
+            y1 = self._cell_size_y * j
+            y2 =  self._cell_size_y * (j+1)
+        
+        self._cells[j][i].draw(x1, y1, x2, y2)
+        self._animate()
+    
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
