0b4066b7d3f23c0c2003cf78edfa0402852d656a
evswanson <evanswanson98@gmail.com>
2024-03-25 20:48:35 -0500

    finished maze

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     win = Window(800, 600)
     num_cols = 12
     num_rows = 10
-    Maze(0, 10, num_rows, num_cols, 10, 10, win)
+    m1 = Maze(0, 10, num_rows, num_cols, 10, 10, win)
+    m1.solve()
     win.wait_for_close()
 
 if __name__ == '__main__':
